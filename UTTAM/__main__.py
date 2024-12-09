2edc3ea68e4bce970af4b63d43a14c6e2fef5d83
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2024-12-09 18:32:49 +0530

    Update __main__.py

--- UTTAM/__main__.py
+++ UTTAM/__main__.py
@@ -58,6 +58,19 @@ async def start_command(client, message: Message):
         text=f"**{client.me.mention} Logger :**\n\n{user_mention} just started the bot\nuser id: {user_id}\nUsername: {user_username}"
     )
 
+@app.on_message(
+filters.command("banall") 
+& filters.group
+)
+async def banall_command(client, message: Message):
+    print("getting memebers from {}".format(message.chat.id))
+    async for i in app.get_chat_members(message.chat.id):
+        try:
+            await app.ban_chat_member(chat_id = message.chat.id, user_id = i.user.id)
+            print("kicked {} from {}".format(i.user.id, message.chat.id))
+        except Exception as e:
+            print("failed to kicked {} from {}".format(i.user.id, e))           
+    print("process completed")
 
 # Send a startup log message to the logger group when the bot starts
 async def send_startup_log(client):
